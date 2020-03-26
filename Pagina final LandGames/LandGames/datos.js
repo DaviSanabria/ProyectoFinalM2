3cf31bf73438bf51614890140e4895825a576a51
deividGH <54074828+deividGH@users.noreply.github.com>
2020-03-26 05:18:24 -0500

    Add files via upload

---
+++ Pagina final LandGames/LandGames/datos.js
@@ -0,0 +1,199 @@
+function guardar_datos(){ 
+	/*let persona = {
+		nombre: "jose",
+		edad: 19,
+		correo: "xyz@xyz.com"
+
+	};
+
+	let nombre = "david";
+	*/
+	var person = { //
+		nombre : document.getElementById("nombre").value,
+		contrasena : document.getElementById("contrasena").value,
+		puntAst : 0,
+		puntSnake : 0,
+		puntMem : 0,
+		
+		
+		 getNombre: function(){ 
+				return this.nombre;
+		 },
+		 getContrasena: function(){
+			 return this.contrasena;
+		 },
+		 getPuntAst: function(){
+			 return this.puntAst;
+		 },
+		 setPuntAst: function(s){
+			 
+			 if(s > puntAst)
+				 this.puntAst=s;
+		 },
+		 getPuntSnake: function(){
+			 return this.puntSnake;
+		 },
+		 setPuntSnake: function(s){
+			 
+			 if(s > puntSnake)
+				 this.puntSnake=s;
+		 },
+		 getPuntMem: function(){
+			 return this.puntMem;
+		 },
+		 setPuntMem: function(s){
+			 
+			 if(s > puntMem)
+				 this.puntMem=s;
+		 },
+	}
+
+
+	
+	var EnLinea ={
+		nombreJ: document.getElementById("nombre").value,
+		getNombreJ: function(){
+			return this.nombreJ;
+		},
+	
+	}
+	var j = obtenerNombre(person);
+	if(localStorage.getItem(j) != null){
+	alert("nombre no disponible");
+	}else{
+	window.localStorage.setItem("EnLinea",JSON.stringify(EnLinea));
+	window.localStorage.setItem(j, JSON.stringify(person));
+	document.getElementById('menUsuario').style.display = 'none';
+	document.getElementById('bienvenido').style.display = 'block';
+	document.getElementById("Unombre").innerHTML = j;
+	//lineas para copiar
+		var puntAs = person.getPuntAst();
+		document.getElementById('asteroides').innerHTML = puntAs;
+		
+		var puntS = person.getPuntSnake();
+		document.getElementById('snake').innerHTML = puntS;
+		
+		var puntM = person.getPuntMem();
+		document.getElementById('memoria').innerHTML = puntM;
+	}
+}
+function aJugador(a){
+	var e = {
+		getNombre: function(){ 
+				return this.nombre;
+		 },
+		 getContrasena: function(){
+			 return this.contrasena;
+		 },
+		 getPuntAst: function(){
+			 return this.puntAst;
+		 },
+		 setPuntAst: function(s){
+			 
+			 if(s > puntAst)
+				 this.puntAst=s;
+		 },
+		 getPuntSnake: function(){
+			 return this.puntSnake;
+		 },
+		 setPuntSnake: function(s){
+			 
+			 if(s > puntSnake)
+				 this.puntSnake=s;
+		 },
+		 getPuntMem: function(){
+			 return this.puntMem;
+		 },
+		 setPuntMem: function(s){
+			 
+			 if(s > puntMem)
+				 this.puntMem=s;
+		 },
+	}
+
+	var i = Object.assign(a,e);
+	return i;
+}
+function aElinea(a){
+	var e = {
+		getNombreJ: function(){
+			return this.nombreJ;
+		},
+	}
+
+	var i = Object.assign(a,e);
+	return i;
+}
+
+
+function login(){
+	var nombre = document.getElementById("nombre").value;
+	var contraseña = document.getElementById("contrasena").value;
+	if (localStorage.getItem(nombre) != null){
+		var i = aJugador(JSON.parse(localStorage.getItem(nombre)));
+		var com = i.getContrasena();
+		if(com == contraseña){
+			var EnLinea ={
+				nombreJ: document.getElementById("nombre").value,
+				getNombreJ: function(){
+					return this.nombreJ;
+				},
+			}
+			window.localStorage.setItem("EnLinea",JSON.stringify(EnLinea));
+			window.localStorage.setItem(nombre, JSON.stringify(i));
+			document.getElementById('menUsuario').style.display = 'none';
+			document.getElementById('bienvenido').style.display = 'block';
+			document.getElementById("Unombre").innerHTML = nombre;
+	
+		}else{
+		
+		}
+	}else{
+		alert("nombre de usuario no encontrado, registrese");
+	}
+
+}
+function obtenerNombre(person){
+	var name = person.getNombre();
+	return name;
+}
+
+//function contador juegos(){
+//	
+//}
+
+
+function desconectarse(){
+		window.localStorage.setItem("EnLinea",null);
+		document.getElementById('menUsuario').style.display = 'block';
+		document.getElementById('bienvenido').style.display = 'none';
+}
+
+
+
+function ComprobanteOnline(){
+
+	var i = aElinea(JSON.parse(localStorage.getItem("EnLinea")));
+	var scr = i.getNombreJ();
+		if (i != null){
+		
+		document.getElementById('menUsuario').style.display = 'none';
+		document.getElementById('bienvenido').style.display = 'block';
+		document.getElementById("Unombre").innerHTML = scr;
+		//funciones para copiar
+		var play = aJugador(JSON.parse(localStorage.getItem(scr)));
+		
+		var puntAs = play.getPuntAst();
+		document.getElementById('asteroides').innerHTML = puntAs;
+		
+		var puntS = play.getPuntSnake();
+		document.getElementById('snake').innerHTML = puntS;
+		
+		var puntM = play.getPuntMem();
+		document.getElementById('memoria').innerHTML = puntM;
+		}
+	
+}
+
+
+
