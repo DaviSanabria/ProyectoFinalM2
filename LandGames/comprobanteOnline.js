1db45b97a3bf36ae71de4a829195d65fd3720fe7
DaviSanabria <54325511+DaviSanabria@users.noreply.github.com>
2020-03-25 19:32:42 -0500

    Add files via upload

--- LandGames/comprobanteOnline.js
+++ LandGames/comprobanteOnline.js
@@ -1,5 +1,8 @@
-	 var i = parseInt(localStorage.getItem("EnCuenta"), 10);
-	if (i == 1){
-	document.getElementById('menUsuario').style.display = 'none';
-	}
+	
+
+  
+
+  
+
+
 
--- LandGames/datos.js
+++ LandGames/datos.js
@@ -1,9 +1,4 @@
-
-
-
 function guardar_datos(){ 
-
-	
 	/*let persona = {
 		nombre: "jose",
 		edad: 19,
@@ -13,31 +8,107 @@ function guardar_datos(){
 
 	let nombre = "david";
 	*/
-const person = {
-     nombre : document.getElementById("nombre").value,
-	 contrasena : document.getElementById("contrasena").value,
-}
-var nombre= getNombre(JSON.stringify(person));
+	var person = {
+		nombre : document.getElementById("nombre").value,
+		contrasena : document.getElementById("contrasena").value,
+		puntAst : "",
+		puntSnk : "",
+		puntMem : "",
+	 
+		
+		 getNombre: function(){
+				return this.nombre;
+		 },
+		 getContrasena: function(){
+			 return this.nombre;
+		 },
 
-window.localStorage.setItem("EnCuenta","1");
-window.localStorage.setItem("user", JSON.stringify(person));
-document.getElementById('menUsuario').style.display = 'none';
+		 
+		 setPuntSmk: function(i){
+			 if (this.puntSnk < i)
+				 this.puntSnk = i;
+		 },
+		 setPuntAst: function(i){
+			 if (this.puntAst < i)
+				 this.puntAst = i;
+		 },
+		 
+		 setPuntMem: function(i){
+			 if (this.puntMem < i)
+				 this.puntMem = i;
+		 },
+	 
+	}
+
 	
+	var EnLinea ={
+		nombreJ: document.getElementById("nombre").value,
+		getNombreJ: function(){
+			return this.nombreJ;
+		},
+	
+	}
+	var j = obtenerNombre(person);
+	if(localStorage.getItem(j) != null){
+	alert("nombre no disponible");
+	}else{
+	window.localStorage.setItem("EnLinea",JSON.stringify(EnLinea));
+	window.localStorage.setItem(j, JSON.stringify(person));
+	document.getElementById('menUsuario').style.display = 'none';
+	document.getElementById('bienvenido').style.display = 'block';
+	document.getElementById("Unombre").innerHTML = j;
+	}
+
+
+
+
+	
+
 }
-function getNombre( person ){
+
+
+function login(){
+	var nombre = document.getElementById("nombre").value;
+	var contraseña = document.getElementById("contrasenia").value;
 	
-	
+		
+
 }
+function obtenerNombre(person){
+	var name = person.getNombre();
+	return name;
+}
+
+
+//function contador juegos(){
+//	
+//}
 
 
 function ocultar(){
-	document.getElementById('menUsuario').style.display = 'none';
-	
-	
+	document.getElementById('menUsuario').style.display = 'none';	
 }
 
-function mostrar(){
+function desconectarse(){
+		window.localStorage.setItem("EnLinea",null);
 		document.getElementById('menUsuario').style.display = 'block';
-
+		document.getElementById('bienvenido').style.display = 'none';
 }
 
+
+function ComprobanteOnline(){
+	var e = {
+		getNombreJ: function(){
+			return this.nombreJ;
+		},
+		
+	}
+	var i = Object.assign(JSON.parse(localStorage.getItem("EnLinea")), e);
+	var scr = i.getNombreJ();
+		if (i != null){
+		
+		document.getElementById('menUsuario').style.display = 'none';
+		document.getElementById('bienvenido').style.display = 'block';
+		document.getElementById("Unombre").innerHTML = scr;
+		}
+}
